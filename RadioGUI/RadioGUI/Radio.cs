e7270e814163bd2e87fe3b71055e8626d233f781
Vasudev Vijayaraman <35582753+vvijayaraman0822@users.noreply.github.com>
2019-03-16 17:24:05 -0500

    Add files via upload

---
+++ RadioGUI/RadioGUI/Radio.cs
@@ -0,0 +1,66 @@
+﻿// Vasudev Vijayaraman
+// C# Programming - 09/02/2018
+// Dr. Catherine Stringfellow
+// Simple Radio GUI using GroupBox, CheckBoxes, TrackBar, RadioButton and PictureBox
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
